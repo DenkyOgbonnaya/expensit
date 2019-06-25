d70d1ec849a4ebfbd76c6c7aa022f1f657c758c8
DenkyOgbonnaya <denkyogb@gmail.com>
2019-06-25 22:07:08 +0100

    add changes needed for deployment to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./server/models/index')
 const expenseRouter =  require('./server/routes/expenseRoute');
+const path = require ('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080; //listing port
@@ -9,12 +10,19 @@ const PORT = process.env.PORT || 8080; //listing port
 app.use(express.json());
 app.use(cors({credentials:true, origin: 'http://localhost:3000'}));
 app.use(express.static(__dirname + '/client/public'));
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.use('/api', expenseRouter);
 
-app.use('/api', expenseRouter)
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '/client/build/index.html'));
+    })
+}
 
-app.get('*', (req, res) => res.status(200).send({
-    message: 'Welcome to Expensit.',
-  }));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/public/build/index.html'));
+})
 
   
     app.listen(PORT, (err) =>{
