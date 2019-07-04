eccbe496c370f4da4ee70c6f86c22617bb750f56
DenkyOgbonnaya <denkyogb@gmail.com>
2019-07-04 01:05:27 +0100

    fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/api', expenseRouter);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use('/static', express.static(path.join(__dirname, 'client/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '/client/build/index.html'));
     })
